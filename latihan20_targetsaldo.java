63dfe3ba84ab9a3181f01f96a9c6aeb0cbebe19b
FatihMuzaqi <145949071+FatihMuzaqi@users.noreply.github.com>
2023-10-28 20:57:37 +0700

    commit

---
+++ latihan20_targetsaldo.java
@@ -0,0 +1,30 @@
+package tipedata;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class latihan20_targetsaldo {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int saldoawal;
+        int bunga;
+        int saldotarget;
+        
+        System.out.print("saldo awal:");
+        saldoawal = scanner.nextInt();
+        System.out.print("Bunga perbulan %:");
+        bunga = scanner.nextInt();
+        System.out.print("saldo target:");
+        saldotarget = scanner.nextInt();
+        
+        int bulan = (saldotarget-saldoawal)/(saldoawal*bunga/100);
+       
+        
+        for(int i = 1;i<= bulan;i++) {
+            saldoawal = saldoawal + (saldoawal * bunga/100);
+            DecimalFormat decimalFormat = new DecimalFormat("#,###");
+            String formattedsaldoawal = decimalFormat.format(saldoawal);
+            System.out.println("Saldo di bulan ke- " +i+" Rp "+ formattedsaldoawal);
+        }
+    
+    }
+    
+}
